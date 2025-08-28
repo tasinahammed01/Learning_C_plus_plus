bb3709622f57bb8f2d711ea7bf32fb043753f444
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-08-28 23:04:44 +0600

    problem 4 completed

--- Patterns/squrePattern.cpp
+++ Patterns/squrePattern.cpp
@@ -1,3 +1,57 @@
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     int n;
+
+//     cin >> n;
+
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         for (int j = 0; j < n; j++)
+//         {
+//             cout << ("*");
+//         }
+//             cout << ("\n");
+
+//     }
+// }
+
+// printing character
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     int n;
+
+//     cin >> n;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         char ch = 'A';
+
+//         for (int j = 0; j < n; j++)
+//         {
+//             cout << ch;
+//             ch += 1;
+//         }
+//         cout << "\n";
+//     }
+// }
+
+
+// Printing number
+
+
+
 #include <iostream>
 using namespace std;
 
@@ -5,17 +59,19 @@ int main()
 {
 
     int n;
+    int num =0;
 
     cin >> n;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
 
-        for (int j = 0; j < n; j++)
+        for (int j = 1; j <= n; j++)
         {
-            printf("*");
+            num+=1;
+            cout << num;
         }
-            printf("\n");
+            cout << ("\n");
 
     }
 }
