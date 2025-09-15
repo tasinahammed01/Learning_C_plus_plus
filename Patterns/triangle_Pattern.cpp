225c470aa4f55da6bbd41cb657b7077a4b5a5706
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-09-15 12:30:57 +0600

    floyd, inverted triangle  learnd

--- Patterns/triangle_Pattern.cpp
+++ Patterns/triangle_Pattern.cpp
@@ -1,3 +1,69 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+
+//     int n;
+//     cin >> n;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j < i + 1; j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//         }
+
+//     return 0;
+// }
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+//  1
+//  1 2
+//  1 2 3 
+//  1 2 3 4
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+
+//     int n;
+//     cin >> n;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j < i + 1; j++)
+//         {
+//             cout << j;
+//         }
+//         cout << endl;
+//         }
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+//  1
+//  2 1
+//  3 2 1 
+//  4 3 2 1
+
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
@@ -8,9 +74,9 @@ int main()
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i + 1; j++)
+        for (int j = i+1 ; j > 0; j--)
         {
-            cout << "*";
+            cout << j << " ";
         }
         cout << endl;
         }
