1d9dc64d55c3d27b44f931872038f038d44ae13c
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-09-15 17:01:21 +0600

    fibonacci using faction recursion

---
+++ Functions/fibonacchi.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fib(int a);
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    cout << fib(n);
+
+    return 0;
+}
+
+int fib(int a)
+{
+
+    if (a == 0)
+        return 0;
+    if (a == 1)
+        return 1;
+
+    return fib(a - 1) + fib(a - 2);
+}
