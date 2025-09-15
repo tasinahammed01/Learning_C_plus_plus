84fd1c9fe7e4d8ab5d7842f2b8c2ef032ae9efd9
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-09-15 15:01:12 +0600

    binomial coefficiant problem solve

---
+++ Functions/binomial_coefficiant.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int a);
+int BinomalCoefficiant(int n, int r);
+
+int main()
+{
+
+    int n, r;
+    cin >> n >> r;
+
+    cout << BinomalCoefficiant(n, r);
+
+    return 0;
+}
+
+int factorial(int a)
+{
+    int fact = 1;
+    for (int i = 1; i <= a; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+int BinomalCoefficiant(int n, int r)
+{
+    int fact_N = factorial(n);
+    int fact_R = factorial(r);
+
+    return (fact_N / (factorial(n - r)* fact_R));
+}
